6801f5479239ed5faf99c6e05c7d77e013b6c537
JerkyTreats <public@jerkytreats.dev>
2022-04-06 22:14:14 -0700

    Add WIP protobuf contract for BoardData

--- Assets/Scripts/BoardData.cs
+++ Assets/Scripts/BoardData.cs
@@ -5,7 +5,50 @@ using ProtoBuf;
 
 [ProtoContract]
 public struct BoardData {
+
+    [ProtoMember(1)]
     public Vector3Int spawnPosition;
+
+    [ProtoMember(2)]
     public Vector2Int boardSize;
-    public List<Tile> tiles;
+
+    [ProtoMember(3)]
+    public List<BoardTileData> tiles;
+}
+
+[ProtoContract]
+public struct BoardTileData {
+    public Block block;
+    public Vector3Int position;
+}
+
+[ProtoContract]
+public enum Block {
+    Blue,
+    Cyan,
+    Ghost,
+    Green,
+    Orange,
+    Purple,
+    Red,
+    Yellow,
+}
+
+[ProtoContract]
+public struct Vector3IntSurrogate
+{
+    [ProtoMember(1)]
+    public int X { get; set; }
+
+    [ProtoMember(2)]
+    public int Y { get; set; }
+
+    [ProtoMember(3)]
+    public int Z { get; set; }
+
+    public static implicit operator Vector3Int(Vector3IntSurrogate vector) =>
+        new Vector3Int(vector.X, vector.Y, vector.Z);
+
+    public static implicit operator Vector3IntSurrogate(Vector3Int vector) =>
+        new Vector3IntSurrogate { X = vector.x, Y = vector.y, Z = vector.z };
 }
