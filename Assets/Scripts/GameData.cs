e84b10299ea1d6b1a7a53fc29b7753f3264762b1
JerkyTreats <public@jerkytreats.dev>
2022-04-10 07:06:43 -0700

    Merge GameData and GameInitializer objects

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,8 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using ProtoBuf.Meta;
 
+/// <summary>
+/// Holds Game and Initialization data for the game.
+/// Tiles/Data objects are populated in editor, passed through to game objects.
+/// </summary>
 public class GameData : MonoBehaviour
 {
     public Tile ghostTile;
@@ -10,5 +13,22 @@ public class GameData : MonoBehaviour
     public TetrominoData[] tetrominos;
 
     public BorderPieceData[] borderPieces;
+
+    void Awake () {
+        ProtoBufSerializationSetup();
+    }
+
+    /// <summary>
+    /// Intended to run once, and only once, when the game is first started.
+    /// From https://blog.oliverbooth.dev/2021/04/27/writing-a-portable-save-load-system/
+    /// <para>
+    /// Protobuf doesn't understand UnityEngine.Vector3Int (or any Unity type).
+    /// Therefore we've created a surrogate type. Here we tell protobuf to use the surrogate in place of the Unity type
+    /// </summary>
+    private void ProtoBufSerializationSetup() {
+        var model = RuntimeTypeModel.Default;
+        model.Add<Vector3IntSurrogate>();
+        model.Add<Vector3>(false).SetSurrogate(typeof(Vector3IntSurrogate));
+    }
 
 }
